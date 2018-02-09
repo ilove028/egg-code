6a3796366bbf15416949c6b189917c473fc9c559
mx <xuhegn.mao@timanetworks.com>
2018-02-09 10:18:31 +0800

    processive step 2

--- config/plugin.js
+++ config/plugin.js
@@ -1,8 +1,8 @@
 const path = require('path');
 
 module.exports = {
-    // ua: {
-    //     enable: true,
-    //     path: path.join(__dirname, '../lib/plugin/egg-ua')
-    // }
+    ua: {
+        enable: true,
+        path: path.join(__dirname, '../lib/plugin/egg-ua')
+    }
 };
